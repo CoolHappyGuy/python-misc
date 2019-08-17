c9a090ae75abe8870a938eb72e74d0e3bc621670
CoolHappyGuy <daveandsuejones@gmail.com>
2019-08-17 15:50:38 -0500

    Module 5 Completion

--- BlockChain/blockchain.py
+++ BlockChain/blockchain.py
@@ -1,16 +1,27 @@
+import _functools
 # Initializing our (empty) blockchain list
 MINING_REWARD = 10 # Global Constant (Reward given for person who mines a new block)
+# Our starting block for the blockchain
 genesis_block = {
         'previous_hash': '',
         'index': 0,
         'transactions': []
 }
+# Initializing our (empty) blockchain list
 blockchain = [genesis_block]
+# Unhandled transactions
 open_transactions = []
+# We are the owner of this blockchain node, hence this is our identifier (e.g. for sending coins)
 owner = 'CoolHappyGuy'
+# Registered participants: Ourself + other people sending/ receiving coins
 participants = {'CoolHappyGuy'}
 
 def hash_block(block):
+    """Hashes a block and returns a string representation of it.
+
+        Arguments:
+            :block: The block that should be hashed.
+        """
     return '-'.join([str(block[key]) for key in block])
 
 
@@ -51,13 +62,20 @@ def add_transaction(recipient, sender=owner, amount=1.0):
 
 
 def mine_block():
+    """Create a new block and add open transactions to it."""
+    # Fetch the currently last block of the blockchain
     last_block = blockchain[-1]
+    # Hash the last block (=> to be able to compare it to the stored hash value)
     hashed_block =  hash_block(last_block)
+    # Miners should be rewarded, so let's create a reward transaction
     reward_transaction = {
         'sender': 'MINING',
         'recipient': owner,
         'amount':  MINING_REWARD
     }
+    # Copy transaction instead of manipulating the original open_transactions list
+    # This ensures that if for some reason the mining should fail, we don't have the reward transaction stored in the
+    # open transactions
     copied_transactions = open_transactions[:]
     open_transactions.append(reward_transaction)
     block = {
@@ -70,19 +88,27 @@ def mine_block():
 
 
 def get_balance(participant):
+    """Calculate and return the balance for a participant.
+
+        Arguments:
+            :participant: The person for whom to calculate the balance.
+        """
+    # Fetch a list of all sent coin amounts for the given person (empty lists are returned if the person was NOT the sender)
+    # This fetches sent amounts of transactions that were already included in blocks of the blockchain
     tx_sender = [[tx['amount'] for tx in block['transactions'] if tx['sender'] == participant] for block in blockchain]
+    # Fetch a list of all sent coin amounts for the given person (empty lists are returned if the person was NOT the sender)
+    # This fetches sent amounts of open transactions (to avoid double spending)
     open_tx_sender = [tx['amount'] for tx in open_transactions if tx['sender'] == participant]
     tx_sender.append(open_tx_sender)
-    amount_sent = 0
-    for tx in tx_sender:
-        if len(tx) > 0:
-            amount_sent += tx[0]
+    amount_sent = _functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0,
+                                    tx_sender, 0)
+    # This fetches received coin amounts of transactions that were already included in blocks of the blockchain
+    # We ignore open transactions here because you shouldn't be able to spend coins before the transaction was confirmed + included in a bloc
     tx_recipient = [[tx['amount'] for tx in block['transactions'] if tx['recipient'] == participant]
                     for block in blockchain]
-    amount_received = 0
-    for tx in tx_recipient:
-        if len(tx) > 0:
-            amount_received += tx[0]
+    amount_received = _functools.reduce(lambda tx_sum, tx_amt: tx_sum + sum(tx_amt) if len(tx_amt) > 0 else tx_sum + 0,
+                                        tx_recipient, 0)
+    # Return the total balance
     return amount_received - amount_sent
 
 
@@ -180,7 +206,7 @@ while waiting_for_input:
         print('Invalid blockchain!')
         # Break out of the loop
         break
-    print(get_balance('CoolHappyGuy'))
+    print('Balance of {}:  {:6.2f}'.format('CoolHappyGuy', get_balance('CoolHappyGuy')))
 else:
     print('User left!')
 
